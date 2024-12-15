7003a64818834b6c9c47bcef2649a078134c765e
iamadahn <vendonism16@yandex.ru>
2024-12-15 18:45:59 +0300

    display: a function to create complete data widget

--- app/src/display/display.c
+++ app/src/display/display.c
@@ -29,8 +29,20 @@ LV_FONT_DECLARE(jetbrains_14);
 LV_FONT_DECLARE(jetbrains_18);
 
 struct frame_widget {
-    char* text;
+    char *text;
     lv_point_t line_points[6];
+    lv_style_t style;
+    lv_color_t color;
+};
+
+struct data_widget {
+    char *text;
+    lv_obj_t *data_current_label;
+    lv_obj_t *data_min_label;
+    lv_obj_t *data_max_label;
+    lv_point_t main_line_points[4];
+    lv_point_t left_line_points[3];
+    lv_point_t right_line_points[3];
     lv_style_t style;
     lv_color_t color;
 };
@@ -124,6 +136,110 @@ static void frame_widget_create(struct frame_widget *widget,
     widget->line_points[5].y = y_start;
 
     lv_obj_t *line = line_create(widget->line_points, &widget->style, widget->color, 2, 6);
+}
+
+static void data_widget_create(struct data_widget *widget,
+                               unsigned int x_start,
+                               unsigned int y_start)
+{
+    /*-----------*/
+    /* Main line */
+    /*-----------*/
+    unsigned char main_line_x_length = 62,
+        main_line_y_length = 25;
+
+    unsigned int main_line_x_start = x_start,
+        main_line_x_end = main_line_x_start + main_line_x_length,
+        main_line_x_center = main_line_x_end - (main_line_x_length / 2),
+
+        main_line_y_start = y_start + 35,
+        main_line_y_end = main_line_y_start + main_line_y_length;
+
+    widget->main_line_points[0].x = main_line_x_start;
+    widget->main_line_points[0].y = main_line_y_start;
+
+    widget->main_line_points[1].x = main_line_x_end;
+    widget->main_line_points[1].y = main_line_y_start;
+
+    widget->main_line_points[2].x = main_line_x_center;
+    widget->main_line_points[2].y = main_line_y_start;
+
+    widget->main_line_points[3].x = main_line_x_center;
+    widget->main_line_points[3].y = main_line_y_end;
+
+    lv_style_init(&widget->style);
+    lv_obj_t *main_line = line_create(widget->main_line_points, &widget->style, widget->color, 1, 4);
+
+    /*------------*/
+    /* Text label */
+    /*------------*/
+    unsigned char label_pixel_size = strlen(widget->text) * JETBRAINS_12_WIDTH;
+    unsigned char label_space_length = 5;
+    unsigned char label_opacity = 2;
+
+    unsigned int text_label_x = main_line_x_start + label_space_length + label_opacity,
+        text_label_y = y_start;
+
+    lv_obj_t *text_label = label_create(widget->text, &jetbrains_12, widget->color, text_label_x, text_label_y);
+
+    /*-----------*/
+    /* Left line */
+    /*-----------*/
+    unsigned int left_line_x_start = text_label_x - label_opacity,
+        left_line_y_start = text_label_y + 4;
+
+    widget->left_line_points[0].x = left_line_x_start;
+    widget->left_line_points[0].y = left_line_y_start;
+
+    widget->left_line_points[1].x = main_line_x_start;
+    widget->left_line_points[1].y = left_line_y_start;
+
+    widget->left_line_points[2].x = main_line_x_start;
+    widget->left_line_points[2].y = left_line_y_start + label_space_length;
+
+    lv_obj_t *left_out_line = line_create(widget->left_line_points, &widget->style, widget->color, 1, 3);
+
+    /*------------*/
+    /* Right line */
+    /*------------*/
+    unsigned int right_line_x_start = text_label_x + label_pixel_size + label_opacity,
+        right_line_y_start = text_label_y + 4;
+
+    widget->right_line_points[0].x = right_line_x_start;
+    widget->right_line_points[0].y = right_line_y_start;
+
+    widget->right_line_points[1].x = main_line_x_end;
+    widget->right_line_points[1].y = right_line_y_start;
+
+    widget->right_line_points[2].x = main_line_x_end;
+    widget->right_line_points[2].y = right_line_y_start + label_space_length;
+
+    lv_obj_t *right_out_line = line_create(widget->right_line_points, &widget->style, widget->color, 1, 3);
+
+    /*--------------------*/
+    /* Current data label */
+    /*--------------------*/
+    char* data_label_text = "test";
+    unsigned int data_current_label_x = main_line_x_start + 10,
+                data_current_label_y = text_label_y + (SCR_BORDER_OPACITY * 1.5f) + label_opacity;
+
+    widget->data_current_label = label_create(data_label_text, &jetbrains_18, widget->color, data_current_label_x, data_current_label_y);
+
+    /*----------------*/
+    /* Min data label */
+    /*----------------*/
+    unsigned int data_min_label_x = main_line_x_start + 5,
+                data_min_label_y = main_line_y_start + 7;
+
+    widget->data_min_label = label_create(data_label_text, &jetbrains_12, widget->color, data_min_label_x, data_min_label_y);
+
+    /*----------------*/
+    /* Max data label */
+    /*----------------*/
+    unsigned int data_max_label_x = main_line_x_center + label_opacity,
+                data_max_label_y = main_line_y_start + 7;
+
+    widget->data_max_label = label_create(data_label_text, &jetbrains_12, widget->color, data_max_label_x, data_max_label_y);
 }
 
 void display_handler(void *, void *, void *)
