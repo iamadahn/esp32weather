20e7660078333ccac4b70819162e824e25de2af7
iamadahn <vendonism16@yandex.ru>
2025-01-25 20:13:10 +0300

    forecast: parsing all needed data, calculating min, max and current values for each field

--- app/src/forecast/forecast.c
+++ app/src/forecast/forecast.c
@@ -3,6 +3,8 @@
 #include "wifi.h"
 #include "cjson/cJSON.h"
 
+#include <float.h>
+
 #include <zephyr/kernel.h>
 #include <zephyr/net/socket.h>
 #include <zephyr/data/json.h>
@@ -20,108 +22,14 @@ K_MSGQ_DEFINE(forecast_async_state_msgq, sizeof(unsigned short), 1, 1);
 
 unsigned char recv_buf[3072];
 
-static int forecast_response_parse(char *response);
+static int socket_setup(const char* server, const char* port, int *sock);
+static int forecast_get(const char *server, const char *api_key);
 static void response_cb(struct http_response *response,
                         enum http_final_call final_data,
                         void *user_data);
-static int forecast_get(const char *server, const char *api_key);
-static int socket_setup(const char* server, const char* port, int *sock);
-
-static int forecast_response_parse(char *response)
-{
-    cJSON_Hooks hooks = {
-        .malloc_fn = malloc,
-        .free_fn = free,
-    };
-
-    cJSON_InitHooks(&hooks);
-
-    cJSON *json = cJSON_Parse(response);
-    if (json == NULL) {
-        LOG_ERR("Error parsing JSON");
-        return -1;
-    }
-
-    cJSON *timezone = cJSON_GetObjectItemCaseSensitive(json, "timezone");
-    if (cJSON_IsString(timezone) && (timezone->valuestring != NULL)) {
-        LOG_INF("Timezone: %s", timezone->valuestring);
-    }
-
-    cJSON *latitude = cJSON_GetObjectItemCaseSensitive(json, "latitude");
-    if (cJSON_IsNumber(latitude)) {
-        LOG_INF("Latitude: %f", latitude->valuedouble);
-    }
-
-    cJSON *daily = cJSON_GetObjectItemCaseSensitive(json, "daily");
-
-    cJSON *temperature_2m_max = cJSON_GetObjectItemCaseSensitive(daily, "temperature_2m_max");
-    if (cJSON_IsArray(temperature_2m_max)) {
-        LOG_INF("Temperature 2m max - %f", cJSON_GetArrayItem(temperature_2m_max, 0)->valuedouble);
-    }
-
-    cJSON_Delete(json);
-
-    /*
-    while (k_msgq_put(&temperature_outside_msgq, &forecast.current.temperature_2m, K_NO_WAIT) != 0) {
-        k_msgq_purge(&temperature_outside_msgq);
-    }
-
-    while (k_msgq_put(&humidity_outside_msgq, &forecast.current.relative_humidity_2m, K_NO_WAIT) != 0) {
-        k_msgq_purge(&humidity_outside_msgq);
-    }
-    */
-
-    return 0;
-}
-
-static void response_cb(struct http_response *response, enum http_final_call final_data, void *user_data)
-{
-    if (final_data == HTTP_DATA_MORE) {
-        LOG_INF("Partial data received (%zd bytes).", response->data_len);
-    } else if (final_data == HTTP_DATA_FINAL) {
-        LOG_INF("All the data received (%zd bytes).", response->data_len);
-    }
-
-    LOG_INF("GET response status - %s.", response->http_status);
-
-    forecast_response_parse(strchr(response->recv_buf, '{'));
-    
-    unsigned char forecast_async_state = 1;
-    k_msgq_purge(&forecast_async_state_msgq);
-    int ret = k_msgq_put(&forecast_async_state_msgq, &forecast_async_state, K_NO_WAIT);
-    if (ret != 0) {
-        LOG_ERR("Failed to push to wifi state queue");
-    }
-}
-
-static int forecast_get(const char *server, const char *api_key)
-{
-    int sock;
-    struct sockaddr_in addr;
-    int timeout = 10 * MSEC_PER_SEC;
-
-    int ret = socket_setup(server, "80", &sock);
-    if (sock < 0) {
-        LOG_ERR("Failed to establish HTTP connection.");
-    }
-
-    struct http_request req;
-    memset(&req, 0, sizeof(req));
-
-    req.method = HTTP_GET;
-    req.url = api_key;
-    req.host = server;
-    req.protocol = "HTTP/1.1";
-    req.response = response_cb;
-    req.recv_buf = recv_buf;
-    req.recv_buf_len = sizeof(recv_buf);
-
-    ret = http_client_req(sock, &req, timeout, NULL);
-
-    zsock_close(sock);
-
-    return ret;
-}
+static int forecast_response_parse(char *response);
+static int forecast_array_get_min_max(cJSON *array, struct forecast_data *data);
+static int forecast_array_get_current(cJSON *array, struct forecast_data *data, unsigned int current_hour);
 
 static int socket_setup(const char* server, const char* port, int *sock)
 {
@@ -157,6 +65,140 @@ static int socket_setup(const char* server, const char* port, int *sock)
     }
 
     zsock_freeaddrinfo(res);
+    return 0;
+}
+
+static int forecast_get(const char *server, const char *api_key)
+{
+    int sock;
+    struct sockaddr_in addr;
+    int timeout = 10 * MSEC_PER_SEC;
+
+    int ret = socket_setup(server, "80", &sock);
+    if (sock < 0) {
+        LOG_ERR("Failed to establish HTTP connection.");
+    }
+
+    struct http_request req;
+    memset(&req, 0, sizeof(req));
+
+    req.method = HTTP_GET;
+    req.url = api_key;
+    req.host = server;
+    req.protocol = "HTTP/1.1";
+    req.response = response_cb;
+    req.recv_buf = recv_buf;
+    req.recv_buf_len = sizeof(recv_buf);
+
+    ret = http_client_req(sock, &req, timeout, NULL);
+
+    zsock_close(sock);
+
+    return ret;
+}
+
+static void response_cb(struct http_response *response, enum http_final_call final_data, void *user_data)
+{
+    if (final_data == HTTP_DATA_MORE) {
+        LOG_INF("Partial data received (%zd bytes).", response->data_len);
+    } else if (final_data == HTTP_DATA_FINAL) {
+        LOG_INF("All the data received (%zd bytes).", response->data_len);
+    }
+
+    LOG_INF("GET response status - %s.", response->http_status);
+
+    forecast_response_parse(strchr(response->recv_buf, '{'));
+    
+    unsigned char forecast_async_state = 1;
+    k_msgq_purge(&forecast_async_state_msgq);
+    int ret = k_msgq_put(&forecast_async_state_msgq, &forecast_async_state, K_NO_WAIT);
+    if (ret != 0) {
+        LOG_ERR("Failed to push to wifi state queue");
+    }
+}
+
+static int forecast_response_parse(char *response)
+{
+    cJSON_Hooks hooks = {
+        .malloc_fn = malloc,
+        .free_fn = free,
+    };
+
+    cJSON_InitHooks(&hooks);
+
+    cJSON *json = cJSON_Parse(response);
+    if (json == NULL) {
+        LOG_ERR("Error parsing JSON");
+        return -1;
+    }
+
+    cJSON *current = cJSON_GetObjectItemCaseSensitive(json, "current");
+    cJSON *temperature_current = cJSON_GetObjectItemCaseSensitive(current, "temperature_2m");
+    cJSON *humidity_current = cJSON_GetObjectItemCaseSensitive(current, "relative_humidity_2m");
+    cJSON *wind_speed_current = cJSON_GetObjectItemCaseSensitive(current, "wind_speed_10m");
+
+    cJSON *hourly = cJSON_GetObjectItemCaseSensitive(json, "hourly");
+    cJSON *temperature_array = cJSON_GetObjectItemCaseSensitive(hourly, "temperature_2m");
+    cJSON *humidity_array = cJSON_GetObjectItemCaseSensitive(hourly, "relative_humidity_2m");
+    cJSON *wind_speed_array = cJSON_GetObjectItemCaseSensitive(hourly, "wind_speed_10m");
+    cJSON *uvi_array = cJSON_GetObjectItemCaseSensitive(hourly, "uv_index");
+
+    struct forecast forecast;
+    forecast_array_get_min_max(temperature_array, &forecast.temperature);
+    forecast_array_get_min_max(humidity_array, &forecast.humidity);
+    forecast_array_get_min_max(wind_speed_array, &forecast.wind_speed);
+    forecast_array_get_min_max(uvi_array, &forecast.uvi);
+
+    if (cJSON_IsNumber(temperature_current))
+        forecast.temperature.current = temperature_current->valuedouble;
+    else
+        LOG_ERR("Error parsing current temperature");
+
+    if (cJSON_IsNumber(humidity_current))
+        forecast.humidity.current = temperature_current->valuedouble;
+    else
+        LOG_ERR("Error parsing current humidity");
+    /* TODO - need to initialise rtc and ntp and use correct time */
+    forecast_array_get_current(wind_speed_array, &forecast.wind_speed, 0);
+    forecast_array_get_current(uvi_array, &forecast.uvi, 0);
+    /*------------------------------------------------------------*/
+
+    cJSON_Delete(json);
+
+    return 0;
+}
+
+static int forecast_array_get_min_max(cJSON *array, struct forecast_data *data)
+{
+    if (!cJSON_IsArray(array)) {
+        LOG_ERR("Not an array");
+        return -1;
+    }
+
+    double min = FLT_MAX, max = -FLT_MAX;
+
+    for (size_t i = 0; i < 24; i++) {
+        if (cJSON_GetArrayItem(array, i)->valuedouble < min)
+            min = cJSON_GetArrayItem(array, i)->valuedouble;
+        if (cJSON_GetArrayItem(array, i)->valuedouble > max)
+            max = cJSON_GetArrayItem(array, i)->valuedouble;
+    }
+
+    data->max = max;
+    data->min = min;
+
+    return 0;
+}
+
+static int forecast_array_get_current(cJSON *array, struct forecast_data *data, unsigned int current_hour)
+{
+    if (!cJSON_IsArray(array)) {
+        LOG_ERR("Not an array");
+        return -1;
+    }
+
+    data->current = cJSON_GetArrayItem(array, current_hour)->valuedouble;
+
     return 0;
 }
 
--- app/src/forecast/forecast.h
+++ app/src/forecast/forecast.h
@@ -1,6 +1,19 @@
 #ifndef FORECAST_H_
 #define FORECAST_H_ 
 
+struct forecast_data {
+    double max;
+    double min;
+    double current;
+};
+
+struct forecast {
+    struct forecast_data temperature;
+    struct forecast_data humidity;
+    struct forecast_data wind_speed;
+    struct forecast_data uvi;
+};
+
 void forecast_handler(void *, void *, void *);
 
 #endif
