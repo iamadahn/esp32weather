c6fa43ef1755df447b65566f93f5bc0a456c0c09
iamadahn <vendonism16@yandex.ru>
2025-02-04 21:03:35 +0300

    app: goodbye, my dear threads, I will miss you

--- app/src/display/display.h
+++
@@ -1,6 +0,0 @@
-#ifndef DISPLAY_H_
-#define DISPLAY_H_ 
-
-void display_handler(void *, void *, void *);
-
-#endif
--- app/src/ths/ths.h
+++
@@ -1,6 +0,0 @@
-#ifndef THS_H_
-#define THS_H_
-
-void ths_handler(void *, void *, void *);
-
-#endif
